4431e6423a37ab35948ab995f47e65a59e41b962
Tj <newtj2@gmail.com>
2015-10-06 21:17:43 +0100

    A bit of revision
    Changed some classes after testing different container sizes.   Put JS
    function into a variable for name spacing and reverted offsets to normal
    values.

--- js/shinyOne.js
+++ js/shinyOne.js
@@ -1,73 +1,80 @@
-function addShine(elem){
+var shinyObjects = {
 
-    elem.on('mousemove', function (event) {
+    addShine: function (elem) {
 
-        var width = $(this).width(),
-            height = $(this).height();
-        var layers = $(this).find('div').not('.shiny-object').not('.shine-effect');
-        var shinyObject = $(this).find('.shiny-object');
-        var posX = event.pageX - $(this).offset().left,
-            posY = event.pageY - $(this).offset().top;
-        var offsetX = (width * 2) - event.pageX,
-            offsetY = (height *2) - event.pageY;
+        elem.on('mousemove', function (event) {
 
-        var tranY = -1 - (posX / 140);
-        //var tranY2 = 1 - ((-offsetX - 10) / 280);
-        //higher the multiplication, the more the movement created
-        var rotX = 1 - (posX * 3) / width;
-        var rotY = 1 - (posY * 3) / height;
+            var width = $(this).width(),
+                height = $(this).height();
+            var layers = $(this).find('div').not('.shiny-object').not('.shine-effect');
+            var shinyObject = $(this).find('.shiny-object');
+            var shinyEffect = $(this).find('.shine-effect');
+            var posX = event.pageX - $(this).offset().left,
+                posY = event.pageY - $(this).offset().top;
+            var offsetX = (width * 2) - event.pageX,
+                offsetY = (height * 2) - event.pageY;
 
-        var transformTranslate = 'translateY(' + tranY + 'px)';
-        var transformRotate = 'rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)';
+            var tranY = -1 - (posX / 140);
+            //var tranY2 = 1 - ((-offsetX - 10) / 280);
+            //3 should be max with tblr @ -20px
+            var rotX = 1 - (posX * 3) / width;
+            var rotY = 1 - (posY * 3) / height;
 
-        shinyObject.css({transform:transformTranslate + transformRotate});
-        var top = parseInt(layers.css('top'));
-        console.log(top);
+            var transformTranslate = 'translateY(' + tranY + 'px)';
+            var transformRotate = 'rotateX(' + rotX + 'deg) rotateY(' + rotY + 'deg)';
 
-        layers.each(function () {
+            shinyObject.css({transform: transformTranslate + transformRotate});
+            var top = parseInt(layers.css('top'));
 
-            var layerElement = $(this);
-            var offsetLayer = parseInt(layerElement.data('offset')) || 0;
-            //math need to cater for neg and pos values
-            if(offsetLayer > 0){
-                if(offsetLayer > -top *3){
-                    var val = -top  + -top + top;
-                    console.log('off to highx '+offsetLayer+ ' changing to '+ val / 2);
-                    offsetLayer = val / 2;
+            layers.last().css({
+                    '-webkit-filter': 'blur(2px',
+                    'filter': 'blur(2px)'
+                });
+
+            layers.each(function () {
+
+                var layerElement = $(this);
+                var offsetLayer = parseInt(layerElement.data('offset')) || 0;
+                //math need to cater for neg and pos values
+                if (offsetLayer > 0) {
+                    if (offsetLayer > -top * 3) {
+                        var val = -top + -top + top;
+                        //console.log('off to highx '+offsetLayer+ ' changing to '+ val / 2);
+                        offsetLayer = val / 2;
+                    }
+                } else {
+                    if (offsetLayer < (top / 2)) {
+                        //console.log('off to high '+offsetLayer+ ' changing to '+ top / 2);
+                        offsetLayer = top / 2;
+                    }
+
                 }
-            } else {
-                if(offsetLayer < (top / 2)){
-                    console.log('off to high '+offsetLayer+ ' changing to '+ top / 2);
-                    offsetLayer = top / 2;
-                }
+                //if(offsetLayer < (top / 2) || offsetLayer < (-top  + top * 2)){
+                //    console.log('off to high '+offsetLayer+ ' changing to '+ top / 2);
+                //    offsetLayer = top / 2;
+                //}
+                var transformLayer = 'translateX(' + rotX * offsetLayer + 'px) translateY(' + rotY * offsetLayer + 'px)';
 
-            }
-            //if(offsetLayer < (top / 2) || offsetLayer < (-top  + top * 2)){
-            //    console.log('off to high '+offsetLayer+ ' changing to '+ top / 2);
-            //    offsetLayer = top / 2;
-            //}
-            var transformLayer = 'translateX(' + rotX * offsetLayer + 'px) translateY(' + rotY * offsetLayer + 'px)';
+                layerElement.css({transform: transformLayer});
+            });
 
-            layerElement.css({transform: transformLayer});
+            var radian = Math.atan2(posY - height / 2, posX - width / 2);
+            var angle = radian * 180 / Math.PI - 90;
+
+            shinyEffect.css('background', 'linear-gradient(' + angle + 'deg, rgba(255,255,255,' + event.pageY / height + ') 0%,rgba(255,255,255,0) 80%)');
         });
 
-        var radian = Math.atan2(posY - height / 2, posX - width / 2);
-        var angle = radian * 180 / Math.PI - 90;
+        elem.on('mouseleave',function(){
+            var layers = $(this).find('div').not('.shiny-object').not('.shine-effect');
 
-        $('.shine-effect').css('background', 'linear-gradient(' + angle + 'deg, rgba(255,255,255,' + event.pageY / height + ') 0%,rgba(255,255,255,0) 80%)');
+            layers.last().css({
+                    '-webkit-filter': 'blur(0px',
+                    'filter': 'blur(0px)'
+                });
 
-        //debug output
-        $("#coords").html(posX + ',' + posY);
-        $("#coords2").html(offsetX + ',' + offsetY);
-        $("#epx").html(event.pageX);
-        $("#epy").html(event.pageY);
-        $("#theta").html(radian);
-        $("#angle").html(angle);
-        $("#tranY").html(tranY);
-        $("#rotX").html(rotX);
-        $("#rotY").html(rotY);
-    });
+        });
 
-}
+    }
 
+};
 
